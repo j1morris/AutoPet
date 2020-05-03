7f5fe429c2034f45d382487022fec861fc9abf50
Justin Morris <j1morris@ucsd.edu>
2020-05-03 13:13:30 -0700

    added data

--- getCameraData.py
+++ getCameraData.py
@@ -4,14 +4,21 @@ import cv2
 cap = cv2.VideoCapture()
 cap.open("http://192.168.20.11:8080/stream/video.mjpeg")
 
-# Capture frame-by-frame
-ret, frame = cap.read()
+for i in range(25):
 
-# Display the resulting frame
-cv2.imwrite('test.jpg',frame)
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    resized = cv2.resize(frame, (1280,720))
+    cv2.imshow('Frame', resized)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # Display the resulting frame
+    cv2.imwrite('./data/posSamples/sample' + str(i) + '.jpg',resized)
 
 # When everything done, release the capture
 cap.release()
 
-output.write(site.read())
-output.close()
+#output.write(site.read())
+#output.close()
